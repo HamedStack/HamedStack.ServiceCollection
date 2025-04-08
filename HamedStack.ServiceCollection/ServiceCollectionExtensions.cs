23b7c28b8c7ac5eec65787546b220c93d9d689c8
hamedfathi <hamedfathi@outlook.com>
2025-04-08 10:44:21 +0200

    feat: new extensions added.

--- HamedStack.ServiceCollection/ServiceCollectionExtensions.cs
+++ HamedStack.ServiceCollection/ServiceCollectionExtensions.cs
@@ -4,6 +4,7 @@
 namespace HamedStack.ServiceCollection;
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
 using System.Linq;
 
@@ -50,6 +51,38 @@ public static class ServiceCollectionExtensions
         service = serviceProvider.GetService<T>();
         return service != null;
     }
+
+    /// <summary>
+    /// Attempts to find a service of the specified type in the service collection.
+    /// </summary>
+    /// <typeparam name="T">The type of the service to look for.</typeparam>
+    /// <param name="services">The IServiceCollection to search.</param>
+    /// <param name="descriptor">The found service descriptor, if any.</param>
+    /// <returns>True if the service was found; otherwise, false.</returns>
+    public static bool TryGetService<T>(
+        this IServiceCollection services,
+        out ServiceDescriptor? descriptor)
+    {
+        descriptor = services.FirstOrDefault(sd => sd.ServiceType == typeof(T));
+        return descriptor != null;
+    }
+
+    /// <summary>
+    /// Attempts to find a service of the specified type in the service collection.
+    /// </summary>
+    /// <param name="services">The IServiceCollection to search.</param>
+    /// <param name="serviceType">The type of the service to look for.</param>
+    /// <param name="descriptor">The found service descriptor, if any.</param>
+    /// <returns>True if the service was found; otherwise, false.</returns>
+    public static bool TryGetService(
+        this IServiceCollection services,
+        Type serviceType,
+        out ServiceDescriptor? descriptor)
+    {
+        descriptor = services.FirstOrDefault(sd => sd.ServiceType == serviceType);
+        return descriptor != null;
+    }
+
 
     /// <summary>
     /// Removes the first occurrence of a service of the specified type from the <see cref="IServiceCollection"/>.
@@ -212,4 +245,77 @@ public static class ServiceCollectionExtensions
         }
         return services;
     }
+
+    /// <summary> 
+    /// Replace the services identified by <typeparamref name="TService"/> with the implementation provided by <typeparamref name="TImplementation"/>. 
+    /// </summary> 
+    /// <param name="serviceCollection">The design-time service collection.</param> 
+    /// <typeparam name="TService">The type of the service to replace.</typeparam> 
+    /// <typeparam name="TImplementation">The implementation type for the replaced service.</typeparam> 
+    public static void Replace<TService, TImplementation>(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.Replace(typeof(TService), typeof(TImplementation));
+    }
+
+    /// <summary> 
+    /// Replace the services identified by <paramref name="serviceType"/> with the implementation provided by <paramref name="implementationType"/>. 
+    /// </summary> 
+    /// <param name="serviceCollection">The design-time service collection.</param> 
+    /// <param name="serviceType">The type of the service to replace.</param> 
+    /// <param name="implementationType">The implementation type for the replaced service.</param> 
+    public static void Replace(this IServiceCollection serviceCollection, Type serviceType, Type implementationType)
+    {
+        foreach (var originalServiceDescriptor in serviceCollection.Where(e => e.ServiceType == serviceType).ToList())
+        {
+            var serviceDescriptor = new ServiceDescriptor(serviceType, implementationType, originalServiceDescriptor.Lifetime);
+            serviceCollection.Replace(serviceDescriptor);
+        }
+    }
+
+    /// <summary>
+    /// Tries to add a service to the collection using a factory if it is not already registered.
+    /// </summary>
+    /// <typeparam name="TService">The type of the service to add.</typeparam>
+    /// <param name="services">The service collection.</param>
+    /// <param name="implementationFactory">The factory to create the service.</param>
+    /// <param name="lifetime">The lifetime of the service.</param>
+    /// <returns>The modified service collection.</returns>
+    public static IServiceCollection TryAdd<TService>(this IServiceCollection services, Func<IServiceProvider, TService> implementationFactory, ServiceLifetime lifetime)
+        where TService : class
+    {
+        if (services.All(sd => sd.ServiceType != typeof(TService)))
+        {
+            var descriptor = new ServiceDescriptor(typeof(TService), implementationFactory, lifetime);
+            services.Add(descriptor);
+        }
+        return services;
+    }
+
+    /// <summary>
+    /// Removes all registrations of the specified service type.
+    /// </summary>
+    /// <typeparam name="TService">The type of the service to remove.</typeparam>
+    /// <param name="services">The service collection.</param>
+    /// <returns>The modified service collection.</returns>
+    public static IServiceCollection RemoveAll<TService>(this IServiceCollection services)
+    {
+        var serviceDescriptors = services.Where(d => d.ServiceType == typeof(TService)).ToList();
+        foreach (var descriptor in serviceDescriptors)
+        {
+            services.Remove(descriptor);
+        }
+        return services;
+    }
+
+    /// <summary>
+    /// Checks if any implementation of a given interface is registered.
+    /// </summary>
+    /// <typeparam name="TService">The service interface type.</typeparam>
+    /// <param name="services">The service collection.</param>
+    /// <returns>True if any implementation is registered; otherwise, false.</returns>
+    public static bool HasImplementationOf<TService>(this IServiceCollection services)
+    {
+        return services.Any(sd => sd.ImplementationType != null && typeof(TService).IsAssignableFrom(sd.ImplementationType));
+    }
+
 }
